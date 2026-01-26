f8c3b10063a089d2482d76181283926563bfbf51
comefan <364988317@qq.com>
2026-01-26 22:38:08 +0800

    新增axios等

---
+++ src/utils/request.js
@@ -0,0 +1,38 @@
+import axios from 'axios'
+
+//创建一个axios对象处理
+const request = axios.create({
+    baseURL: 'http://localhost:8080',
+    timeout: 5000
+})
+
+//request 拦截器
+//可以自请求发送前对请求做一些处理
+//比如统一加token，对请求参数统一加密
+request.interceptors.request.use(config =>{
+    config.headers['Content-Type'] = 'application/json;charset=utf-8';
+
+    //config.headers['token'] = user.token; //设置请求头
+    return config;
+},errror =>{
+    return Promise.reject(errror)
+})
+
+//response 拦截器
+//可以在接口响应后统一处理结果
+request.interceptors.response.use(
+    response => {
+        // response.data 即为后端返回的Result
+        let res = response.data;
+        //兼容服务器返回的字符串数据
+        if(typeof res === 'string'){
+            res = res? JSON.parse(res):res
+        }
+        return res;
+    },error =>{
+        console.log('err:' + error)
+        return Promise.reject(error)
+    }
+)
+
+export default request;
---
+++ src/views/UserView.vue
@@ -0,0 +1,78 @@
+<template>
+  <div>
+    <div style="margin-bottom: 20px">
+      <el-input
+        placeholder="请输入用户名"
+        size="medium"
+        v-model="searchText"
+        style="width: 200px; margin-right: 10px"
+      ></el-input>
+      <el-button type="warning" size="mini">查询</el-button>
+      <el-button type="primary" size="mini">新增</el-button>
+    </div>
+    <el-table :data="tableData" style="width: 100%">
+      <el-table-column prop="date" label="日期"> </el-table-column>
+      <el-table-column prop="name" label="姓名"> </el-table-column>
+      <el-table-column label="操作">
+        <el-button type="primary" size="mini">编辑</el-button>
+        <el-button type="danger" size="mini">删除</el-button>
+      </el-table-column>
+    </el-table>
+    <div class="block" style="margin-top: 20px;">
+      <el-pagination
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        :current-page="currentPage"
+        :page-sizes="[5, 10, 20, 40]"
+        :page-size="pageSize"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="total"
+      >
+      </el-pagination>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      searchText: "",
+      total: 40,
+      currentPage: 1,
+      pageSize: 5,
+      tableData: [
+        {
+          date: "2016-05-02",
+          name: "马红霞",
+          address: "上海市普陀区金沙江路 1518 弄",
+        },
+        {
+          date: "2016-05-04",
+          name: "马玉珍",
+          address: "上海市普陀区金沙江路 1517 弄",
+        },
+        {
+          date: "2016-05-01",
+          name: "王宇",
+          address: "上海市普陀区金沙江路 1519 弄",
+        },
+        {
+          date: "2016-05-03",
+          name: "宋笑天",
+          address: "上海市普陀区金沙江路 1516 弄",
+        },
+      ],
+    };
+  },
+  methods: {
+    handleSizeChange(val) {
+      this.pageSize = val;
+      this.handleCurrentChange(this.currentPage);
+    },
+    handleCurrentChange(val) {
+      this.currentPage = val;
+    },
+  },
+};
+</script>
