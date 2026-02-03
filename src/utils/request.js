9981420b375eb45a988a2bfa463651f40c35dd79
comefan <364988317@qq.com>
2026-02-03 22:42:34 +0800

    接口添加 token

--- src/utils/request.js
+++ src/utils/request.js
@@ -11,8 +11,10 @@ const request = axios.create({
 //比如统一加token，对请求参数统一加密
 request.interceptors.request.use(config =>{
     config.headers['Content-Type'] = 'application/json;charset=utf-8';
-
-    //config.headers['token'] = user.token; //设置请求头
+    const user = JSON.parse(localStorage.getItem('user'));
+    if(user){
+        config.headers['token'] = user.token; //设置请求头
+    }
     return config;
 },errror =>{
     return Promise.reject(errror)
